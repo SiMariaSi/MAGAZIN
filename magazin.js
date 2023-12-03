5d3d97a5bef4689793dde40a588213b154d29861
SiMariaSi <149849976+SiMariaSi@users.noreply.github.com>
2023-12-03 21:55:24 +0300

    Update magazin.js

--- magazin.js
+++ magazin.js
@@ -27,20 +27,7 @@ async function getResponse() {
 //        console.log(content[key])
 //    }
 
-    content_price=content.sort((a, b) => a.price - b.price);
-
-    let node_for_insert = document.getElementById("node_for_insert")
-    for (key in content_price) {
-        node_for_insert.innerHTML += `
-        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
-        <img style="width: 180px" class="align-self-center" src=${content[key].img}>
-        <h5 class="card-title">${content[key].title}</h5>
-        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick="this.value = this.checked ? 1 : 0"> </p>
-        </li>
-                `
-    }
+    
 }
 
 async function getResponse1() {
